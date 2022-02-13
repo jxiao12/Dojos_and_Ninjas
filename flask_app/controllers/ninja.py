3d89026ba4c983f1a0f1fc81b420688aaaf39d36
j5xiao <81590932+j5xiao@users.noreply.github.com>
2022-02-12 20:51:42 -0800

    b

---
+++ flask_app/controllers/ninja.py
@@ -0,0 +1,15 @@
+from flask import render_template, request, redirect, Response
+
+from flask_app import app
+from flask_app.models.ninja import Ninja
+
+@app.route('/ninjas')
+def ninjas():
+    
+    return render_template('ninja.html',dojos= dojo.Dojo.get_all())
+
+
+@app.route('/create/ninja',methods=['POST'])
+def create_ninja():
+    ninja.Ninja.save(request.form)
+    return redirect('/')
